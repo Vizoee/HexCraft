4ced15322048c503a8d12855e55fba8a52e4b032
Vizoee <193793023+Vizoee@users.noreply.github.com>
2025-01-10 23:40:39 +0100

    fix

--- CC/github.lua
+++ CC/github.lua
@@ -1,5 +1,10 @@
 local function api(url)
-    local response = http.get(url)
+    local apiurl = url
+        :gsub("https://github.com/", "https://api.github.com/repos/")
+        :gsub("https://raw.githubusercontent.com/", "https://api.github.com/repos/")
+        :gsub("blob/main/", "contents/")
+        :gsub("refs/heads/main/", "contents/")
+    local response = http.get(apiurl)
     local json = require("json")
     local content = json.get(response, "content"):gsub("\\n", "\n")
     local name = json.get(response, "name"):gsub(" ", "_")
