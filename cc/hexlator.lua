a436759b65243e1273d77dca6c72de01ab3d654b
Vizoee <193793023+Vizoee@users.noreply.github.com>
2025-02-09 16:49:22 +0100

    test

--- cc/hexlator.lua
+++ cc/hexlator.lua
@@ -203,6 +203,7 @@ local identRegistry = {
         }
         local str = getBalancedParens(s, token["start"])
         local angles = ""
+        print(str:sub(1, 2))
         if str == "v" or str:sub(1, 2) == "v" then
             angles = "a"
         else
