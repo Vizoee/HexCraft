027e2d9d892195daf71cf0f05cee104e5b2ffd65
Vizoee <193793023+Vizoee@users.noreply.github.com>
2025-05-10 19:03:32 +0200

    :3

--- cc/random/randomRandom/events.lua
+++ cc/random/randomRandom/events.lua
@@ -1,4 +1,4 @@
 while true do
-    local event, p1, p2, p3, p4, p5, p6, p7, p8 = os.pullEvent()
-    print(event, p1, p2, p3, p4, p5, p6, p7, p8)
+    local e = {os.pullEvent()}
+    print(table.unpack(e))
 end
